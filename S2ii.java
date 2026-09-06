dfe610eee2e7bd62f1b7e31af9d643bfbb9bbe27
Utkarsh Singh <YOUR_GITHUB_EMAIL>
2026-09-06 22:39:01 +0530

    Add second code

---
+++ S2ii.java
@@ -0,0 +1,15 @@
+public class S2ii {
+    static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+        for(String word : words) {
+            StringBuilder sb = new StringBuilder(word);
+            result.append(sb.reverse().toString()).append(" ");
+        }
+        return result.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseEachWord("hello club")); // olleh bulc
+    }
+}
